778fcf81db173c445744e488dcb1205006fa0ef7
Qin-shihuang <0.0@owo.li>
2024-04-08 19:43:40 +0800

    Add README

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,24 @@
+//! Rust entry point for the kernel
+#![deny(missing_docs)]
+#![deny(warnings)]
+
+#![cfg_attr(target_arch = "mips", feature(asm_experimental_arch))]
 #![no_std]
 #![no_main]
+#![feature(panic_info_message)]
 
 mod panic;
 
-fn main() {
+use core::{arch::global_asm, include_str};
 
+global_asm!(include_str!("init/entry.S"));
+
+/// Entry point for the kernel, called by _entry() in init/entry.S
+#[no_mangle]
+pub fn kernel_init() -> ! {
+
+
+    loop {
+
+    }
 }
