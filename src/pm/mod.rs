c6f058e743fd314c25cb9c6510b9e15793844822
Qin-shihuang <0.0@owo.li>
2024-05-19 13:05:39 +0800

    Fix: fixed include_bytes alignment

--- src/pm/mod.rs
+++ src/pm/mod.rs
@@ -11,18 +11,41 @@ pub mod schedule;
 
 pub static mut ENV_MANAGER: EnvManager = EnvManager::new();
 
-macro_rules! env_create {
-    ($name: ident, $path: expr, $priority: expr) => {
-        let $name = include_bytes!($path);
-        unsafe {
-            ENV_MANAGER.create($name, $priority);
-        }
-    };
-}
-
 pub fn init() {
     unsafe { ENV_MANAGER.init() };
     info!("Process manager initialized.");
-    env_create!(test, "../../user/bare/overflow.b", 1);
     unsafe { schedule(true); }
+}
+
+
+#[repr(C)]
+pub struct AlignedAs<Align, Bytes: ?Sized> {
+    pub _align: [Align; 0],
+    pub bytes: Bytes,
+}
+
+/// Include a file as a byte slice aligned as a specific type.
+#[macro_export]
+macro_rules! include_bytes_align_as {
+    ($align_ty:ty, $path:literal) => {{
+        use $crate::pm::AlignedAs;
+
+        static ALIGNED: &AlignedAs<$align_ty, [u8]> = &AlignedAs {
+            _align: [],
+            bytes: *include_bytes!($path),
+        };
+
+        &ALIGNED.bytes
+    }};
+}
+
+/// Create an environment from a ELF file.
+#[macro_export]
+macro_rules! env_create {
+    ($name: ident, $path: expr, $priority: expr) => {
+        let $name = include_bytes_align_as!(usize, $path);
+        unsafe {
+            $crate::pm::ENV_MANAGER.create($name, $priority);
+        }
+    };
 }
